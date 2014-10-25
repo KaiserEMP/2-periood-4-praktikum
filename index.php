bcf02d536499a1fa92063fe290537fe7f028b954
Kaido Soodla <kaidogta3@hotmail.com>
2014-10-25 21:49:12 +0300

    Finaru

---
+++ index.php
@@ -0,0 +1,37 @@
+	<?php 
+		if (isset($_POST["submit"])) {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$message = "L2ks valesti";
+
+		echo "<style>.pwclass{border: 1px solid orange;}</style>"; }
+		else {
+		$message = "Palun logi sisse";}
+
+	 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>praktikum 2.4</title>
+</head>
+<body>
+
+		<h2><?php echo $message;?></h2>
+			<form action="index.php" method="post">
+
+
+				text<input type="text" name="username" value="<?php if (isset($_POST["submit"])) { $username = $_POST['username'];
+
+				 echo $username;}?>"></input>
+
+				pswrd<input type="password" name="password" class="pwclass" ></input>
+
+				<input type="submit" value="Saada" name="submit"></input>
+
+
+			</form>
+		<pre><?php print_r($_POST);?></pre>
+
+</body>
+</html>
